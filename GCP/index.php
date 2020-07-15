872490e75c55788b75386098f36be492a0b054b6
my-totem <46503988+my-totem@users.noreply.github.com>
2020-07-15 16:06:02 -0400

    Add files via upload

---
+++ GCP/index.php
@@ -0,0 +1,5 @@
+<?php include 'components/authentication.php' ?>     
+<?php include 'components/session-check-index.php' ?>
+<?php include 'controllers/webflow/head.php' ?>
+<?php include 'controllers/navigation/index-before-login-navigation.php' ?>
+<?php include 'components/landing-page-body.php' ?>
